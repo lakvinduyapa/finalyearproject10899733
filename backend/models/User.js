b5bc320ccb89f7a90e70e2c1000e8fb631ba3331
lakvinduyapa <10899733@students.plymouth.ac.uk>
2025-01-26 13:30:53 +0530

    created user model and database connection

---
+++ backend/models/User.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+    {
+        firstname:{
+            type:String,
+            required:true,
+        },
+        lastname:{
+            type:String,
+            required:true,
+        },
+        contact:{
+            type:String,
+            required:true,
+        },
+        email:{
+            type:String,
+            required:true,
+        },
+        password:{
+            type:String,
+            required:true,
+        },
+        orders:[
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref:"Order", 
+            }
+        ],
+        hasShippingAddress:{
+            type:Boolean,
+            default:false,
+        },
+        ShippingAddress:{
+            addressline1:{
+                type:String,
+            },
+            addressline2:{
+                type:String,
+            },
+            city:{
+                type:String,
+            },
+            province:{
+                type:String,
+            },
+            postalcode:{
+                type:String,
+            },
+            country:{
+                type:String,
+            },
+        },
+    },
+    {
+        timestamps:true,
+    }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;

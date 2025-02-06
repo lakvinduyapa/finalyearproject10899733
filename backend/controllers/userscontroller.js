1e374a35d484c0603e54d06530708f332491a83c
lakvinduyapa <10899733@students.plymouth.ac.uk>
2025-02-06 20:04:25 +0530

    hash password created

--- backend/controllers/userscontroller.js
+++ backend/controllers/userscontroller.js
@@ -1,26 +1,28 @@
 import User from "../models/User.js"
-
+import bcrypt from "bcryptjs";
 export const registerusercontroller = async(req, res) =>{
    const{ firstname, lastname, contact, email, password}=req.body;
   //check if user exist
   const userExists = await User.findOne({ email });
   if(userExists){
      res.json({
-        message: 'User already exists',
+        message: "User already exists",
      });
   }
-
+  //hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
   //create user
   const user = await User.create({
      firstname, 
      lastname, 
      contact, 
      email, 
-     password
+     password: hashedPassword,
   });
   res.status(201).json({
      status:'success',
-     message: 'User Registered Successfully',
+     message: "User Registered Successfully",
      data:user,
   })
 

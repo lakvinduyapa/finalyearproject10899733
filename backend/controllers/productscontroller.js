0f0a105f68d2f35b88b001e63c5c09775a1f0488
lakvinduyapa <10899733@students.plymouth.ac.uk>
2025-03-27 11:55:13 +0530

    page filtering and pagination created

--- backend/controllers/productscontroller.js
+++ backend/controllers/productscontroller.js
@@ -27,7 +27,41 @@ export const createproductcontoller = asyncHandler (async (req,res) =>{
 });
 
 export const getproductcontroller = asyncHandler(async(req,res)=>{
-    const products = await Product.find();
+    console.log(req.query);
+    //query
+    let productQuery = Product.find()
+  
+ //search by name
+if (req.query.name){
+    productQuery = productQuery.find({
+        name:{ $regex: req.query.name, $options:"i" },
+    });
+}
+//search by category
+if (req.query.category){
+    productQuery = productQuery.find({
+        name:{ $regex: req.query.category, $options:"i" },
+    });
+}
+//filter by price
+if (req.query.price) {
+    const priceRange = req.query.price.split("-");
+    productQuery = productQuery.find({
+        price: {$gte: priceRange[0], $lte: priceRange[1]},
+    });
+}
+//pagination
+const page = parseInt(req.query.page) ? parseInt(req.query.page) : 1;
+const limit = parseInt(req.query.limit) ? parseInt(req.query.limit) : 10;
+const startIndex = (page - 1) * limit;
+const endIndex = page * limit;
+const total = await Product.countDocuments();
+
+productQuery = productQuery.skip(startIndex).limit(limit);
+
+  //query await
+  const products = await productQuery;
+
     res.json({
         status:"success",
         products,
